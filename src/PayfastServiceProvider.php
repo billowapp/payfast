08edff8499ed2d568827637ce934cc437e40aa6e
Warren Hansen <warren@billow.co.za>
2016-02-25 10:09:15 +0200

    Added Laravel 5 ServiceProvider

---
+++ src/PayfastServiceProvider.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Billow;
+
+
+use Illuminate\Support\ServiceProvider;
+
+class PayfastServiceProvider extends ServiceProvider
+{
+
+    public function register()
+    {
+        // TODO: Implement register() method.
+    }
+
+    public function boot()
+    {
+        // TODO: Implement register() method.
+    }
+
+
+}

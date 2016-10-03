a0836b9b0bb50146b152eaab46d7bb9833e75520
Warren Hansen <warren@billow.co.za>
2016-10-03 18:50:52 +0200

    refactor to env

--- src/config/payfast.php
+++ src/config/payfast.php
@@ -13,10 +13,9 @@ return
 */
 [
     'testing' => true, // Set to false when in production.
-    'currency' => 'ZAR', // ZAR is the only supported currency at this point.
     'merchant' => [
-        'merchant_id' => '10000100', // TEST Credentials. Replace with your merchant ID from Payfast.
-        'merchant_key' => '46f0cd694581a', // TEST Credentials. Replace with your merchant key from Payfast.
+        'merchant_id' => env('PF_MERCHANT_ID', '10000100'), // TEST Credentials. Replace with your merchant ID from Payfast.
+        'merchant_key' => env('PF_MERCHANT_KEY', '46f0cd694581a'), // TEST Credentials. Replace with your merchant key from Payfast.
         'return_url' => env('PF_RETURN_URL', 'http://your-domain.co.za/success'), // Redirect URL on Success.
         'cancel_url' => env('PF_CANCEL_URL', 'http://your-domain.co.za/success'), // Redirect URL on Cancellation.
         'notify_url' => env('PF_ITN_URL', 'http://your-domain.co.za/success'), // ITN URL.

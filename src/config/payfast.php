4b2924df25f504d26347542ebf73d1df2c41c2a3
Surya <surya@webfox.com>
2020-07-09 12:47:12 +1200

    ADD passphrase to config file

--- src/config/payfast.php
+++ src/config/payfast.php
@@ -12,13 +12,13 @@ return
 |
 */
 [
-    'testing' => true, // Set to false when in production.
+    'testing'  => true, // Set to false when in production.
     'merchant' => [
-        'merchant_id' => env('PF_MERCHANT_ID', '10000100'), // TEST Credentials. Replace with your merchant ID from Payfast.
-        'merchant_key' => env('PF_MERCHANT_KEY', '46f0cd694581a'), // TEST Credentials. Replace with your merchant key from Payfast.
-        'return_url' => env('PF_RETURN_URL', 'http://your-domain.co.za/success'), // Redirect URL on Success.
-        'cancel_url' => env('PF_CANCEL_URL', 'http://your-domain.co.za/cancel'), // Redirect URL on Cancellation.
-        'notify_url' => env('PF_ITN_URL', 'http://your-domain.co.za/itn'), // ITN URL.
+        'merchant_id'  => env('PF_MERCHANT_ID', '10000100'),                        // TEST Credentials. Replace with your merchant ID from Payfast.
+        'merchant_key' => env('PF_MERCHANT_KEY', '46f0cd694581a'),                  // TEST Credentials. Replace with your merchant key from Payfast.
+        'return_url'   => env('PF_RETURN_URL', 'http://your-domain.co.za/success'), // Redirect URL on Success.
+        'cancel_url'   => env('PF_CANCEL_URL', 'http://your-domain.co.za/cancel'),  // Redirect URL on Cancellation.
+        'notify_url'   => env('PF_ITN_URL', 'http://your-domain.co.za/itn'),        // ITN URL.
     ],
 
     'hosts' => [
@@ -26,6 +26,8 @@ return
         'sandbox.payfast.co.za',
         'w1w.payfast.co.za',
         'w2w.payfast.co.za',
-    ]
+    ],
+
+    'passphrase' => env('PAYFAST_PASSPHRASE'), // set this in your payfast merchant settings
 
 ];
